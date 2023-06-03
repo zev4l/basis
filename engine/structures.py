e8d0607c8feadeb4363d3caff89971c0d5c1ea23
José Almeida <jose.v.almeida@tecnico.ulisboa.pt>
2023-06-03 22:36:09 +0100

    Patched code formatting

--- engine/structures.py
+++ engine/structures.py
@@ -1,10 +1,12 @@
 import random
 from enum import Enum, IntEnum
 
+
 class Pool:
     """
     Structure to be used by Game, which represents an aggregation of players
-    """    
+    """
+
     def __init__(self):
         self.players = []
         self.current_player_index = 0
@@ -21,11 +23,13 @@ class Pool:
         return None
 
     def set_current_player(self, player):
-        self.current_player_index= self.players.index(player)    
+        self.current_player_index = self.players.index(player)
 
     def advance_player(self):
         if self.players:
-            self.current_player_index = (self.current_player_index + 1) % len(self.players)
+            self.current_player_index = (self.current_player_index + 1) % len(
+                self.players
+            )
 
     def __str__(self):
         return ", ".join([player.name for player in self.players])
@@ -33,12 +37,14 @@ class Pool:
     def __len__(self) -> int:
         return len(self.players)
 
+
 class Rank(IntEnum):
     """
     A helper enum for card ranks.
     Structure: (rank, points)
     Allows comparing ranks between the real rank, and obtaining points via the points property (e.g. Rank.Ace.points -> 11)
     """
+
     Ace = (10, 11)
     Seven = (9, 10)
     King = (8, 4)
@@ -56,40 +62,46 @@ class Rank(IntEnum):
         obj.points = points
         return obj
 
+
 class Suit(Enum):
     """
     A helper enum for card suits
     """
+
     SPADES = "Spades"
     HEARTS = "Hearts"
     DIAMONDS = "Diamonds"
     CLUBS = "Clubs"
 
+
 class Card:
     """
     A representation of a card
     """
-    def __init__(self, rank : Rank, suit : Suit):
+
+    def __init__(self, rank: Rank, suit: Suit):
         self.rank = rank
         self.suit = suit
         self.points = rank.points
 
     def __eq__(self, other):
         return self.rank == other.rank and self.suit == other.suit
-    
+
     def __repr__(self):
-        return f'({self.suit.value}) {self.rank.name}'
-        
+        return f"({self.suit.value}) {self.rank.name}"
+
+
 class Deck:
     """
     A representation of a deck
     """
+
     def __init__(self):
         self.reset()
 
     def reset(self):
         self.cards = []
-        
+
         for suit in Suit:
             for rank in Rank:
                 card = Card(rank, suit)
@@ -116,10 +128,10 @@ class Deck:
         print("Deck:")
         for card in self.cards:
             print(card)
-            
+
     def __len__(self):
         return len(self.cards)
-    
+
 
 class State(Enum):
     RUNNING = 1
