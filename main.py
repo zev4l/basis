2f97f2f8beff7bd4283d60310f1927ad9347fe3f
José Almeida <jose.v.almeida@tecnico.ulisboa.pt>
2023-06-05 17:27:43 +0100

    Added example on how to use a human player without the UI (main.py)

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from engine.structures import Card, Rank, Suit
 from engine.game import Game
-from engine.players import RandomAgent, SimpleGreedyAgent
+from engine.players import Human, RandomAgent, SimpleGreedyAgent
 
 # Test callback
-def test_callback(new_player):
+def player_change_callback(new_player):
     print("Callback called, new player: ", new_player)
+
+# Test user input
+
+def human_input_handler():
+    i = input("Pick a card (1-3): ")
+    return int(i) - 1
 
 player1 = RandomAgent("Player 1")
 player2 = SimpleGreedyAgent("Player 2")
+player3 = Human("Player 3")
+player3.register_input_handler(human_input_handler)
 
 game = Game()
 game.add_player(player1)
 game.add_player(player2)
-game.player_pool.register_callback(test_callback)
+game.add_player(player3)
+game.player_pool.register_callback(player_change_callback)
 game.start_match()
 while not game.is_over():
 	game.next_round()
